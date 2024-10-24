a377db5f96a3c4a9f241a8d8cbd9151776d9fc4e
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-10-24 19:16:18 +0200

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import './bootstrap';
+
+import "~resources/scss/app.scss";
+
+// Images behaviour dir
+
+import.meta.glob([
+    '../img/**'
+]);
+
+// Bootstrap
+
+import * as bootstrap from "bootstrap";
